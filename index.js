c3459986f3b3f5858939cc794864ddc9dd65b252
mahmoudadel54 <mahmoodmadel@gmail.com>
2021-03-08 21:47:53 +0200

    Use more than state + create more than reducer and combine bet. them

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const createStore = require("redux").createStore;
+//implement 2 state - multiple reducers
 
+const createStore = require("redux").createStore;
+const combineReducers = require("redux").combineReducers;
 // action JS Object
 const BUY_CAKE = "BUY_CAKE";
 
@@ -11,14 +13,45 @@ const buyCakeActionCreater = () => {
   };
 };
 
+const BUY_ICECREAM = "BUY_ICECREAM";
+
+const buyIceCreameActionCreater = () => {
+  return {
+    type: BUY_ICECREAM,
+    info: "Buy one ice cream",
+  };
+};
+
 //initial state =-> JS Object
-const initState = {
-  noOfCake: 20,
+// const initState = {
+//   noOfCake: 20,
+// };
+
+const initCakeState = {
+  noOfCake: 10,
+};
+
+const initIceCreamState = {
+  noOfIceCream: 10,
 };
 
 //reducer --> JS Function accepts prevState, action as arguments
 //returns the new state
-const reducer = (state = initState, action) => {
+// const reducer = (state = initState, action) => {
+//   switch (action.type) {
+//     case "BUY_CAKE":
+//       return {
+//         ...state,
+//         noOfCake: state.noOfCake - 1,
+//       };
+//     default:
+//       return state;
+//   }
+// };
+
+//multiple reducers
+//1- cake reducer
+const cakeReducer = (state = initCakeState, action) => {
   switch (action.type) {
     case "BUY_CAKE":
       return {
@@ -30,8 +63,26 @@ const reducer = (state = initState, action) => {
   }
 };
 
+//2- ice cream reducer
+const iceCreamReducer = (state = initIceCreamState, action) => {
+  switch (action.type) {
+    case BUY_ICECREAM:
+      return {
+        ...state,
+        noOfIceCream: state.noOfIceCream - 1,
+      };
+    default:
+      return state;
+  }
+};
+
+//combine reducers in root reducer
+const rootReducer = combineReducers({
+  cake: cakeReducer,
+  iceCream: iceCreamReducer,
+});
 //create store
-const store = createStore(reducer);
+const store = createStore(rootReducer);
 
 // get the state
 console.log(store.getState());
@@ -45,7 +96,9 @@ const unsubscribe = store.subscribe(() =>
 //invoke the action creater
 store.dispatch(buyCakeActionCreater());
 //or invoke direct action
-store.dispatch({ type: "BUY_CAKE" });
+store.dispatch(buyCakeActionCreater());
+store.dispatch(buyIceCreameActionCreater());
+store.dispatch(buyIceCreameActionCreater());
 
 //to handle unsubscribe and prevent subscribe function to fire
 unsubscribe();
